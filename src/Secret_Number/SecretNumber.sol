2b091e620fbe8f50f513d62eb763328ef06dd5b3
navillanueva <nicolasarnedo99@gmail.com>
2024-11-21 18:52:24 +0100

    feat: solution Secret Number

--- src/Secret_Number/SecretNumber.sol
+++ src/Secret_Number/SecretNumber.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract ExploitContract {
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    address payable public targetContract;
+    bytes32 answerHash = 0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365;
 
-    function increment() public {
-        number++;
+
+    function Exploiter() public pure returns (uint8) {
+        for (uint8 i = 0; i < 256; i++) {
+            if (keccak256(abi.encodePacked(i)) ==
+                0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365) {
+                return i;
+            }
+        }
+
+        revert("Secret number not found");
     }
 }
