333553ef0d8777cd9cd86d9bcaa60759ba4f32d3
navillanueva <nicolasarnedo99@gmail.com>
2026-02-04 23:56:30 +0400

    new function

--- zcash-zrc20/zr20.sol
+++ zcash-zrc20/zr20.sol
@@ -1,7 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title ZR20
+ * @dev ZRC-20 style ERC20 token for Zcash-bridged or Zcash-native use cases.
+ */
 contract ZR20 is ERC20 {
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 10 ** 18;
+
     constructor() ERC20("ZR20", "ZR20") {
-        _mint(msg.sender, 1000000000000000000000000);
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
