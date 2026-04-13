cb2c52abb3391cb04ad9b0e104920ba2026ed8ba
navillanueva <nicolasarnedo99@gmail.com>
2026-04-13 14:30:00 +0200

    Minor code improvement

--- zcash-zrc20/zr20.sol
+++ zcash-zrc20/zr20.sol
@@ -3,10 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/**
- * @title ZR20
- * @dev ZRC-20 style ERC20 token for Zcash-bridged or Zcash-native use cases.
- */
+
 contract ZR20 is ERC20 {
     uint256 public constant INITIAL_SUPPLY = 1_000_000 * 10 ** 18;
 
